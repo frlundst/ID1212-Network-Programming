632b8d2835c93500d85acdcdaae29e50c82dc452
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2022-11-09 12:48:54 +0100

    l1

---
+++ L1/src/main/java/com/id1212/l1/L1.java
@@ -0,0 +1,12 @@
+package com.id1212.l1;
+
+/**
+ * 
+ * @author Fredrik Lundström
+ */
+public class L1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
