7563dcaada3928b7b364eb086ebf39c1ff5ed5d8
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2023-01-04 09:19:21 +0100

    removed t_jpa, added new backend project

---
+++ backend_project/src/main/java/com/project/ProjectApplication.java
@@ -0,0 +1,14 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hej");
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
