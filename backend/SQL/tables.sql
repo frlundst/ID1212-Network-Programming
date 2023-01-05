0d685bdaf5a13f626cb563e9d8f05904ab47a5db
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2023-01-05 12:48:40 +0100

    commit

--- backend/SQL/tables.sql
+++ backend/SQL/tables.sql
@@ -14,6 +14,8 @@
 --    FOREIGN KEY (category_id) REFERENCES category(id)
 --);
 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE category (
     id VARCHAR NOT NULL default uuid_generate_v1(),
     name VARCHAR NOT NULL,
