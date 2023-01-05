ca1fd207d76c0da6c772559b465585accec170ba
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2023-01-05 15:26:10 +0100

    Update Product.tsx

--- frontend_project/src/View/Product.tsx
+++ frontend_project/src/View/Product.tsx
@@ -3,7 +3,9 @@ import Wrapper from "../Components/Wrapper";
 import { ProductType } from "../Types";
 import { createUseStyles } from "react-jss";
 import Dot from "../Components/Dot";
-import { MdPersonPin } from "react-icons/md";
+import { BsCheckLg } from "react-icons/bs";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const useStyles = createUseStyles({
     image: {
@@ -30,23 +32,39 @@ interface ProductProps {
 
 function Product(props: ProductProps) {
     const classes = useStyles();
+    const navigate = useNavigate();
+
+    const [addedToCart, setAddedToCart] = React.useState(false);
 
     return <Wrapper>
         <Row>
             <Col>
                 <h1>{props.product?.name}</h1>
-                <h6>Category <span className={classes.category}>{props.product?.category?.name}</span></h6>
+                <h6>
+                    Category <span className={classes.category} onClick={() => navigate(`/category/${props.product.category.id}`)}>
+                        {props.product?.category?.name}
+                    </span>
+                </h6>
                 <br />
                 <Image className={classes.image} src="/images/default.png" />
                 <br />
                 <br />
             </Col>
-            <Col style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+            <Col style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
                 <h5>Price:</h5>
                 <h1>{props.product.price} kr</h1>
-                <Button style={{width: "75%"}}>Add to cart</Button>
+                <Button
+                    style={{ width: "75%" }}
+                    disabled={addedToCart}
+                    onClick={() => {
+                        setAddedToCart(true);
+                        setTimeout(() => {
+                            setAddedToCart(false);
+                        }, 2000);
+                    }}
+                >{addedToCart ? <><BsCheckLg /> Added to cart</> : "Add to cart"}</Button>
                 <div className={classes.stockRow}><Dot size="20px" numberAvailable={props.product?.numberAvailable} /> {props.product?.numberAvailable} in store</div>
-                
+
             </Col>
         </Row>
         <hr />
