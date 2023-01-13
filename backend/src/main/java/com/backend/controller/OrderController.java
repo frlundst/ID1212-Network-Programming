33466300d1358fb831a169cdbf76c53c5a488be1
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2023-01-13 13:46:33 +0100

    Update OrderController.java

--- backend/src/main/java/com/backend/controller/OrderController.java
+++ backend/src/main/java/com/backend/controller/OrderController.java
@@ -1,8 +1,6 @@
 package com.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
